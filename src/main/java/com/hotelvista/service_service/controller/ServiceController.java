9a3e0fa1719ba8179b035494b5f7090c8f262ad0
MyDuyen <22721461.duyen@student.iuh.edu.vn>
2026-05-07 22:37:58 +0700

    update eureka

--- src/main/java/com/hotelvista/service_service/controller/ServiceController.java
+++ src/main/java/com/hotelvista/service_service/controller/ServiceController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/services")
+@RequestMapping("/api/services")
 @CrossOrigin(origins = "http://localhost:5173")
 public class ServiceController {
     private final ServiceCatalogService serviceCatalogService;
